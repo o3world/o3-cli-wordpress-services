d57d94a8c5e5d6aeff61dc5b88c99034d6e082d0
MattSchaffO3 <mattschaff@o3world.com>
2019-12-09 11:01:59 -0500

    initial commit

---
+++ o3-cli-services.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name:     O3 CLI Services
+ * Plugin URI:      https://github.com/o3world/o3-cli-wordpress-services/
+ * Description:     Exposes APIs that assist the use of the O3 CLI with Wordpress projects
+ * Author:          Matt Schaff
+ * Author URI:      https://www.o3world.com/about/team/matt-schaff/
+ * Version:         1.0
+ */
