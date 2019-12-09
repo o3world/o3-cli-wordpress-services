0eef203a9f66c5d149fa10dec14d8b5cdbfd183f
MattSchaffO3 <mattschaff@o3world.com>
2019-12-09 18:02:32 -0500

    Added autoloading & a basic class file structure

--- o3-cli-services.php
+++ o3-cli-services.php
@@ -7,3 +7,32 @@
  * Author URI:      https://www.o3world.com/about/team/matt-schaff/
  * Version:         1.0
  */
+
+use O3CliServices\Controller\Url_List_Controller;
+
+/**
+ * Add action hooks
+ */
+add_action( 'init', 'o3_cli_autoload' );
+add_action( 'rest_api_init', function () {
+  o3_cli_register_url_list();
+} );
+
+/**
+ * Callback for 'init' action
+ */
+function o3_cli_autoload() {
+  require_once dirname(__FILE__) . '/auto-load.php';
+  $loader = new \O3_Cli_Autoloader;
+  $loader->register();
+  $loader->add_namespace('O3CliServices', dirname(__FILE__) . '/src');
+}
+
+/**
+ * Callback for 'rest_api_init' action
+ */
+function o3_cli_register_url_list() {
+  $controller = new Url_List_Controller;
+  $controller->register_route();
+}
+
---
+++ src/Service/Url_List_Manager.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace O3CliServices\Service;
+
+use O3CliServices\Url_List_Plan_Interface;
+
+/**
+ * A generic service that returns paths of posts and menu items based on HTTP
+ * request query parameters
+ */
+class Url_List_Manager {
+
+
+  /**
+   * Build generic test array, to be converted to JSON
+   *
+   * @param Url_List_Plan_Interface $test_plan
+   * @return array
+   */
+  public function build_paths_array(Url_List_Plan_Interface $test_plan) {
+    $node_paths = $this->build_post_paths($test_plan);
+    $menu_paths = $this->build_menu_paths($test_plan);
+    $merged_paths = array_merge($node_paths, $menu_paths);
+    sort($merged_paths);
+    return array();
+  }
+
+  /**
+   * Build the node paths from the provided test plan
+   *
+   * @param Url_List_Plan_Interface $test_plan
+   * @return array $paths
+   */
+  protected function build_post_paths(Url_List_Plan_Interface $test_plan) {
+    $properties = $test_plan->get_properties();
+    $post_data = $this->get_post_type_data('path', $properties['post_types'], $properties['limit']);
+    return $post_data;
+  }
+
+  /**
+   * Get post types, with count of each
+   *
+   * @return array
+   */
+  public function count_posts_by_type() {
+    $post_types = array();
+    return $this->get_post_type_data('count', $post_types);
+  }
+
+  /**
+   * Get post type IDs, depending on count or path
+   *
+   * @param string $data_type
+   * @param array $post_types
+   * @param boolean $limit
+   * @return void
+   */
+  protected function get_post_type_data($data_type = 'path', $post_types, $limit = FALSE) {
+    $extracted_data = array();
+
+    return $extracted_data;
+  }
+
+  /**
+   * Build the node paths from the provided test plan
+   *
+   * @param Url_List_Plan_Interface $test_plan
+   * @return array $paths
+   */
+  protected function build_menu_paths(Url_List_Plan_Interface $test_plan) {
+    // Get the nodes, limit by limit.
+    $properties = $test_plan->get_properties();
+    return isset($properties['menus']) ? $this->get_menu_data($properties['menus']) : array();
+  }
+
+  /**
+   * Count nodes in menus
+   *
+   * @return array
+   */
+  public function countMenuNodes() {
+    $menus = array();
+    return $this->get_menu_data($menus, 'count');
+  }
+
+  /**
+   * Get node either count or path data from menus
+   *
+   * @param string $menus
+   * @param string $data_type
+   *   - Either 'path' or 'count'
+   * @return array
+   */
+  protected function get_menu_data($menus, $data_type = 'path') {
+    $data = array();
+    if (!empty($menus)) {
+      // Load each menu & extract all child links, recursively.
+      foreach ($menus as $menu) {
+        // if ($menu_tree = $this->menuLinkTree->load($menu, new MenuTreeParameters())) {
+        //   $this->extract_paths_from_menu($data, $menu, $menu_tree, $data_type);
+        //   $data = array_filter(array_unique($data));
+        // }
+      }
+    }
+    return $data;
+  }
+
+  /**
+   * A recursive function that extracts link paths in nested menus
+   *s
+   * - Only selects links with internal URLs
+   *
+   * @param array $paths
+   * @param string $menu
+   * @param array $menu_tree
+   * @param string $data_type
+   *   - Either 'path' or 'count'
+   */
+  protected function extract_paths_from_menu(array &$data, string $menu, array $menu_tree, string $data_type) {
+    foreach (array_values($menu_tree) as $item) {
+      // if ($item->link->isEnabled() && !$item->link->getUrlObject()->isExternal()) {
+      //   if ($data_type === 'path') {
+      //     //$dataarray() = $item->link->getUrlObject()->toString(TRUE)->getGeneratedUrl();
+      //   }
+      //   elseif ($data_type === 'count') {
+      //     $data[$menu] = isset($data[$menu]) ? $data[$menu] + 1 : 1;
+      //   }
+      //   if ($item->hasChildren) {
+      //     $this->extractPathsFromMenu($data, $menu, $item->subtree, $data_type);
+      //   }
+      // }
+    }
+  }
+
+}
